7148bfc98115cd6a883023b13bd182857a1db698
yourname <youremail@example.com>
2019-09-04 16:48:23 +0800

    本地化（提示文字汉化）

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vant from 'vant'
-import VeeValidate from 'vee-validate'
+import VeeValidate, { Validator } from 'vee-validate'
+// 导入veevalidate的中文包
+import zhCN from 'vee-validate/dist/locale/zh_CN'
 import 'vant/lib/index.css'
 import './styles/index.less'
 import App from './App.vue'
@@ -10,6 +12,8 @@ import store from './store'
 Vue.use(Vant)
 // 配置插件veevalidate
 Vue.use(VeeValidate)
+// 配置中文
+Validator.localize('zhCN', zhCN)
 Vue.config.productionTip = false
 
 new Vue({
