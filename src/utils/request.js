c394883ccf2fda85599b083727986da3e174fa5b
yourname <youremail@example.com>
2019-09-04 10:00:10 +0800

    json-bigint

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
+import JSONbig from 'json-bigint'
 
 const request = axios.create({
   baseURL: 'http://ttapi.research.itcast.cn'
 })
-
+request.defaults.transformResponse = [function (data) {
+  try {
+    // data 数据可能不是标准的 JSON 格式字符串，否则会导致 JSONbig.parse(data) 转换失败报错
+    return JSONbig.parse(data)
+  } catch (err) {
+    // 无法转换的数据直接原样返回
+    return data
+  }
+}]
 // Add a request interceptor
 request.interceptors.request.use(function (config) {
   // Do something before request is sent
@@ -15,6 +24,7 @@ request.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 request.interceptors.response.use(function (response) {
+  console.log(response)
   // Do something with response data
   return response
 }, function (error) {
