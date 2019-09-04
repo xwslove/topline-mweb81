98602d88b815fad0ba45673c31ac31579e7c87c6
yourname <youremail@example.com>
2019-09-04 09:55:22 +0800

    axios封装

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/utils/request.js
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+const request = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn'
+})
+
+// Add a request interceptor
+request.interceptors.request.use(function (config) {
+  // Do something before request is sent
+  return config
+}, function (error) {
+  // Do something with request error
+  return Promise.reject(error)
+})
+
+// Add a response interceptor
+request.interceptors.response.use(function (response) {
+  // Do something with response data
+  return response
+}, function (error) {
+  // Do something with response error
+  return Promise.reject(error)
+})
+
+export default request
