4e67debf03c49266a247659f97b39cb4f3afaa8e
yourname <youremail@example.com>
2019-09-09 20:06:34 +0800

    弹出层显示的内容

--- src/views/home/components/MoreAction.vue
+++ src/views/home/components/MoreAction.vue
@@ -4,7 +4,20 @@
  :show-confirm-button="false"
  close-on-click-overlay
 >
-hellow
+<van-cell-group v-show="!hide">
+  <van-cell title="拉黑作者" icon="location-o"/>
+  <van-cell title="防窥内容" icon="location-o" is-link @click="hide=true"/>
+  <van-cell title="不感兴趣" icon="location-o"/>
+</van-cell-group>
+
+<van-cell-group v-show="hide">
+  <van-cell icon="arrow-left" @click="hide=false"/>
+  <van-cell title="色情低俗" />
+  <van-cell title="青少那想" />
+  <van-cell title="政治舆论" />
+  <van-cell title="不甘心去" />
+</van-cell-group>
+
 </van-dialog>
 </template>
 
@@ -13,7 +26,8 @@ export default {
   name: 'MoreAction',
   data () {
     return {
-      show: true
+      show: true,
+      hide: false
     }
   }
 }
