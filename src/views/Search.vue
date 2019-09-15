9d8e0a77e870bf0b78ea4772251ffa14afb5ee73
yourname <youremail@example.com>
2019-09-15 20:00:22 +0800

    搜索-历史记录-删除

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -5,7 +5,7 @@
       v-model="value"
       placeholder="请输入搜索关键词"
       show-action
-      @search="onSearch"
+      @search="onSearch(value)"
       @cancel="onCancel"
       @input="handleInput"
       clearable
@@ -14,6 +14,7 @@
     <!-- 搜索提示 -->
     <van-cell-group v-show='value'>
       <van-cell
+      @click="onSearch(item)"
       v-for="item in suggestionList"
       :key="item"
       :title="item"
@@ -24,15 +25,18 @@
     <van-cell-group v-show="!value">
       <van-cell title="历史记录">
         <!-- 自定义右侧内容 -->
-        <div>
+        <div v-show="isEdit" style="display: inline-block" >
           <span>全部删除</span>&nbsp;
-          <span>完成</span>&nbsp;
-          <van-icon name="delete" size="18px" />
+          <span @click="isEdit=false">完成</span>&nbsp;
         </div>
+        <van-icon v-show="!isEdit" @click="isEdit=true" name="delete" size="18px"></van-icon>
       </van-cell>
-      <van-cell title="单元格">
+      <van-cell
+      v-for="(item, index) in histories"
+      :key="item"
+      :title="item">
         <!-- 自定义右侧内容 -->
-        <van-icon name="close" size="18px" />
+        <van-icon v-show="isEdit" name="close"  @click="handleDelete(index)"  size="18px" />
       </van-cell>
     </van-cell-group>
   </div>
@@ -40,16 +44,47 @@
 
 <script>
 import { getSuggestion } from '@/api/search'
+import { mapState } from 'vuex'
+import * as storageTools from '@/utils/localStorage'
 export default {
   name: 'Search',
   data () {
     return {
       value: '',
-      suggestionList: []
+      suggestionList: [],
+      // 编辑模式
+      isEdit: false,
+      // 历史记录
+      histories: []
     }
   },
+  computed: {
+    ...mapState(['user'])
+  },
+  created () {
+    // 加载历史记录
+    if (this.user) {
+      // 从服务器获取数据
+      return
+    }
+    // 没有登录，从本地存储获取数据
+    this.histories = storageTools.getItem('history' || [])
+  },
   methods: {
-    onSearch () {},
+    onSearch (item) {
+      // 判断hisory中是否已经存在item
+      if (this.histories.includes(item)) {
+
+      }
+      // 记录搜索历史
+      this.histories.push(item)
+      // 判断用户是否登录
+      if (this.user) {
+        return
+      }
+      // 没登录把历史存储到本地存储
+      storageTools.setItem('history', this.histories)
+    },
     onCancel () {},
     // 再文本框输入的过程中获取提示
     async handleInput () {
@@ -63,6 +98,10 @@ export default {
       } catch (err) {
         console.log(err)
       }
+    },
+    handleDelete (index) {
+      this.histories.splice(index, 1)
+      storageTools.setItem('history', this.histories)
     }
   }
 }
